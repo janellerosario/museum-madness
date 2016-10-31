5ebd81cbb33e550d49d58d7d6f86a4852ab82a4e
Janelle Rosario <janelle.rosario@gmail.com>
2016-10-31 18:03:03 -0400

    minor changes

--- models/favorites.js
+++ models/favorites.js
@@ -4,7 +4,6 @@ const { getDB }    = require('../lib/dbConnect.js');
 function saveFavMuseum(req,res,next) {
   // creating an empty object for the insertObj
   const insertObj = {};
-
   // copying all of req.body into insertObj
   for(key in req.body) {
     insertObj[key] = req.body[key];
@@ -16,6 +15,7 @@ function saveFavMuseum(req,res,next) {
     db.collection('fav_museums')
       .insert(insertObj.favorite, (insertErr, result) => {
         if (insertErr) return next(insertErr);
+        // return data
         res.savedMuseums = result;
         db.close();
         return next();
@@ -31,6 +31,7 @@ getDB().then((db) => {
       .find({ userId: { $eq: req.session.userId } })
       .toArray((arrayError, data) => {
         if (arrayError) return next(arrayError);
+        // return data
         res.getFavMuseum = data;
         db.close();
         return next();
@@ -45,6 +46,7 @@ function deleteFavMuseum(req, res, next) {
     db.collection('fav_museums')
       .findAndRemove({ _id: ObjectID(req.params.name) }, (removeErr, doc) => {
         if (removeErr) return next(removeErr);
+        // return data
         res.deleteFavMuseum = doc;
         db.close();
         return next();
@@ -60,7 +62,6 @@ function editMuseum(req, res, next) {
       .findAndModify({ _id: ObjectID(req.params.id) }, [] /* sort */,
       { $set: req.body.newmuseum }, { new: true } /* options */, (updateError, doc) => {
         if (updateError) return next(updateError);
-
         // return the data
         res.updated = doc;
         db.close();
@@ -76,7 +77,6 @@ function saveMuseumEdit(req, res, next) {
     db.collection('fav_museums')
       .findOne({ _id: ObjectID(req.params.id) }, (findErr, newmuseum) => {
         if (findErr) return next(findErr);
-
         // return the data
         res.newmuseum = newmuseum;
         db.close();
